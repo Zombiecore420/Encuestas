144633a7e3daac0f1aedb2a445c9b6afe4baa79e
Moisés Maldonado <PleaseDo01@hotmail.com>
2020-06-03 14:33:28 -0500

    Método Agregar completo

--- src/main/java/com/plsdo/demo/service/PersonaService.java
+++ src/main/java/com/plsdo/demo/service/PersonaService.java
@@ -30,8 +30,13 @@ public class PersonaService implements IPersonaService {
 
 	@Override
 	public int save(Persona p) {
-		// TODO Auto-generated method stub
-		return 0;
+		int res=0;
+		Persona persona=data.save(p);
+		if(!persona.equals(null))
+		{
+			res=1;
+		}
+		return res;
 	}
 
 	@Override
