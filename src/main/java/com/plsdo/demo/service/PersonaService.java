c3226d2580ac9985faf9d29661fb80b7e3c7d06b
Maldonado Barrientos Moisés <al221810654@gmail.com>
2020-06-03 03:20:06 -0500

    Creación de PersonaService.Java con sus respectivos métodos

---
+++ src/main/java/com/plsdo/demo/service/PersonaService.java
@@ -0,0 +1,41 @@
+package com.plsdo.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.plsdo.demo.interfaceService.IPersonaService;
+import com.plsdo.demo.interfaces.IPersona;
+import com.plsdo.demo.modelo.Persona;
+
+public class PersonaService implements IPersonaService {
+
+	@Autowired
+	private IPersona data;
+	
+	@Override 
+	public List<Persona> list() {
+		// TODO Auto-generated method stub
+		return (List<Persona>)data.findAll();
+	}
+
+	@Override
+	public Optional<Persona> listarId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int save(Persona p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
