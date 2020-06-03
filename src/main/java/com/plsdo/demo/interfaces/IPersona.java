ce30be13b6ce43990ad86d6f0424b030e2068786
Maldonado Barrientos Moisés <al221810654@gmail.com>
2020-06-03 03:52:36 -0500

    Método Listar completo

--- src/main/java/com/plsdo/demo/interfaces/IPersona.java
+++ src/main/java/com/plsdo/demo/interfaces/IPersona.java
@@ -1,9 +1,11 @@
 package com.plsdo.demo.interfaces;
 
 import org.springframework.data.repository.CrudRepository; //Importa la clase CrudRepository
+import org.springframework.stereotype.Repository;
 
 import com.plsdo.demo.modelo.Persona; //Importa la clase persona
 
+@Repository
 public interface IPersona extends CrudRepository<Persona, Integer> {
 
 }
--- src/main/java/com/plsdo/demo/service/PersonaService.java
+++ src/main/java/com/plsdo/demo/service/PersonaService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.plsdo.demo.interfaceService.IPersonaService;
 import com.plsdo.demo.interfaces.IPersona;
 import com.plsdo.demo.modelo.Persona;
 
+@Service
 public class PersonaService implements IPersonaService {
 
 	@Autowired
