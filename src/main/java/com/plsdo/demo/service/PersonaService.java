170e624994fc997d86c26440d01ecc71df7f3edd
Moisés Maldonado <PleaseDo01@hotmail.com>
2020-06-03 14:44:48 -0500

    Método Editar completo

--- src/main/java/com/plsdo/demo/service/PersonaService.java
+++ src/main/java/com/plsdo/demo/service/PersonaService.java
@@ -24,8 +24,7 @@ public class PersonaService implements IPersonaService {
 
 	@Override
 	public Optional<Persona> listarId(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return data.findById(id);
 	}
 
 	@Override
